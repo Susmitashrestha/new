f192a30d16dbef65c634687b849e18526997eef4
Susmitashrestha <Susmitashrestha365@gmail.com>
2018-05-10 07:56:30 +0545

    seond commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React,{Component} from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
-import Header from './components/Header'
-import Login from './components/Login'
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import Header from './components/Header';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import {BrowserRouter,Switch,Route, Redirect} from 'react-router-dom';
+
+
 //import Table from './components/table'
 class App extends Component{
 	render(){
 		return(
-			
+			<div>
 			 <MuiThemeProvider>
-			
-				<Login />
-			
-             </MuiThemeProvider>
-            
+			 <BrowserRouter>
+				 <Switch>
+					 <Route path="/login" component={Login} />
+					 <Route path="/board" component={Dashboard} />
+				 </Switch>
+			 </BrowserRouter>
+			 </MuiThemeProvider>
+			</div> 
             );
 	}
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,61 +1,103 @@
 import React, {Component} from 'react';
+import {Card, CardActions, CardHeader, CardText,CardMedia,CardTitle} from 'material-ui/Card'
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+//import Avatar from 'material-ui/Avatar';
+import logo from '../assets/logo.jpg';
+import FlatButton from 'material-ui/FlatButton';
+
 
 class Login extends Component 
 {
+	
+	alignCenter = {
+		height: "100vh",          
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center"
+
+	}
+
+	CardWidth = {
+		width: "800px"
+	}
+
+	floatActionButtonRight = {
+		float: "right"
+	}
+
+
+
 	constructor(props){
 		super(props)
-        
-        this.state={
-        	email:'',
-        	Apikey:''
-        }	
+
+		this.state={
+			email:'',
+			Apikey:''
+		}	
 	}
 	render(){
 		return(
-			<div>
+			<div style={this.alignCenter}>
+			<Card style={this.CardWidth}>
 			
-			 Email=<input type='text' placeholder='email'
-			 value={this.state.email}
-			 onChange={(e)=>this.onEmailChange(e)}/><br/>
+			<CardHeader title="Login form"/>
 
-			 Apikey=<input type='password' placeholder='apikey'
-			 value={this.state.Apikey}
-			 onChange={(e)=>this.onApiChange(e)}/><br/>
-			 <button onClick={()=>this.onSubmit()} type="button">Submit</button>
-			  </div>
+			<CardText>
+			Email=<input type='text' placeholder='email'
+			value={this.state.email}
+			onChange={(e)=>this.onEmailChange(e)}/><br/>
+
+			Apikey=<input type='password' placeholder='apikey'
+			value={this.state.Apikey}
+			onChange={(e)=>this.onApiChange(e)}/><br/>
+			</CardText>
+			<CardActions>
+			<FlatButton label ="Submit" onClick={(event)=> this.handleClick(event)}/>
+			</CardActions>
+			</Card>
+			</div>
 			)
-	}
-	onEmailChange(e)
-	{
-			this.setState({email:e.target.value})
-	}
-	onApiChange(e)
-	{
-			this.setState({Apikey:e.target.value})
-	}
-	onSubmit()
-	{
-		//console.log("jdnjnjcdn")
-			
-			fetch('https://api.rebrandly.com/v1/account',
-			{
-				headers:
-				{
-					apikey: this.state.Apikey
-				}
-			})
-			.then(response=>
-			{
-				if(response.ok)
-				{
-					console.log(response.json())
+		}
 
-			    }
-				else
-				{
-					alert(response.statusText)
+		handleClick = function(e){
+
+			fetch('https://api.rebrandly.com/v1/accounts',
+			{
+				headers: {
+					Apikey: this.state.Apikey
 				}
 			})
+			.then(response =>
+			{
+				if(response.ok){
+					response.json()
+					.then(data => {
+						console.log(data);
+						if(data.email === this.state.email){
+							console.log("right user");
+						}
+						else{
+							alert('unauthorized username');
+						}
+					})
+				}
+				else{
+					alert(response.statusText)
+				}	
+			})
+
+		}
+		onEmailChange = function(e){
+			this.setState({
+				email: e.target.value
+			});
+		}
+		onApiChange = function(e){
+			this.setState({
+
+				Apikey: e.target.value
+			});
+		}
 	}
-}
-export default Login;
+	export default Login;
